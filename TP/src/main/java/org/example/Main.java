f38df3ec190f993f5edd7ff1cf6d0f5617e5cc93
brunolofaro-uade <blofaro@uade.edu.ar>
2023-04-23 12:41:00 -0300

    getNelementOfStack without destroying stack

--- TP/src/main/java/org/example/Main.java
+++ TP/src/main/java/org/example/Main.java
@@ -4,9 +4,8 @@ import org.classes.dynamic.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         QueueOfStacks queueOfStacks = new QueueOfStacks(3);
-        trace(queueOfStacks,3);
+        System.out.println(trace(queueOfStacks,3));
         //print queueOfStacks
-        /*
         while(!queueOfStacks.isEmpty()) {
             Stack stack = (Stack) queueOfStacks.getFirst().getValue();
             while(!stack.isEmpty()){
@@ -15,7 +14,7 @@ public class Main {
             }
             System.out.println("\n");
             queueOfStacks.remove();
-        }*/
+        }
     }
 
     static int trace(QueueOfStacks queueOfStacks, int dimensions) throws Exception {
@@ -25,7 +24,7 @@ public class Main {
             Stack stack = (Stack) queueOfStacks.getFirst().getValue();
             tempQueueOfStacks = new Queue(); //store the stacks in a temp queue to restore them.
             tempQueueOfStacks.add(stack);
-            //acum+=getElementInNPosition(queueOfStacks, i);
+            acum+=getElementInNPosition(stack, i);
             queueOfStacks.remove();
             while(!tempQueueOfStacks.isEmpty()){
             queueOfStacks.add(tempQueueOfStacks.getFirst());//restore the stacks
@@ -33,5 +32,35 @@ public class Main {
             }
         }
         return acum;
+    }
+
+    static int getElementInNPosition(Stack stack, int n) throws Exception {
+        Stack tempStack = new Stack();
+        for(int i=0;i<n;i++){
+            tempStack.add(stack.getTop().getValue());
+            stack.remove();
+        }
+        int value = (int) stack.getTop().getValue();
+        for(int i=0;i<n;i++){
+            stack.add(tempStack.getTop().getValue());
+            tempStack.remove();
+        }
+        return value;
+    }
+    static void printStack(Stack stack) throws Exception {
+        Stack temp = new Stack();
+        while(!stack.isEmpty())
+        {
+            int value = (int)stack.getTop().getValue();
+            System.out.println(value);
+            temp.add(value);
+            stack.remove();
+        }
+        while(!temp.isEmpty())
+        {
+            int value = (int) temp.getTop().getValue();
+            stack.add(value);
+            temp.remove();
+        }
     }
 }
