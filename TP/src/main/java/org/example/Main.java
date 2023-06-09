469f2b2ea5f9e320621b70857667ae46c1548fd1
BrunoLoFaro <blofaro@uade.edu.ar>
2023-06-09 15:05:11 -0300

    Montecarlo class

--- TP/src/main/java/org/example/Main.java
+++ TP/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 import org.classes.dynamic.*;
+import org.classes.statics.Coordinate;
+import org.classes.statics.Montecarlo;
+
 import static org.utils.Ex1.*;
 
 import java.util.Random;
---
+++ TP/src/main/java/org/utils/Ex6.java
@@ -0,0 +1,5 @@
+package org.utils;
+import org.classes.statics.Coordinate;
+public class Ex6 {
+
+}
