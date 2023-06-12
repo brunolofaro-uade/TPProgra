4409353b483033472b7dd9decc52ac42f517746b
brunolofaro-uade <blofaro@uade.edu.ar>
2023-06-12 14:25:53 -0300

    ex1 refactor

--- TP/src/main/java/org/classes/dynamic/QueueOfStacks.java
+++ TP/src/main/java/org/classes/dynamic/QueueOfStacks.java
@@ -13,4 +13,39 @@ public class QueueOfStacks<T> extends Queue{
     public int getN(){
         return this.n;
     }
+    public void copy(QueueOfStacks<T> destination) throws Exception {
+        QueueOfStacks<T> tempQueueOfStacks = new QueueOfStacks<>(destination.getN());
+
+        // Copy elements from the original queueOfStacks to the destination queueOfStacks
+        while (!this.isEmpty()) {
+            Stack<T> originalStack = (Stack<T>) this.getFirst().getValue();
+            Stack<T> copiedStack = new Stack<>();
+
+            // Copy elements from the original stack to the copied stack
+            Stack<T> tempStack = new Stack<>();
+            while (!originalStack.isEmpty()) {
+                T value = originalStack.getTop().getValue();
+                copiedStack.add(value);
+                tempStack.add(value);
+                originalStack.remove();
+            }
+
+            // Restore the original stack
+            while (!tempStack.isEmpty()) {
+                originalStack.add(tempStack.getTop().getValue());
+                tempStack.remove();
+            }
+
+            // Add the copied stack to the destination QueueOfStacks
+            tempQueueOfStacks.add(copiedStack);
+            destination.add(copiedStack);
+            this.remove();
+        }
+
+        // Restore the original QueueOfStacks
+        while (!tempQueueOfStacks.isEmpty()) {
+            this.add(tempQueueOfStacks.getFirst().getValue());
+            tempQueueOfStacks.remove();
+        }
+    }
 }
--- TP/src/main/java/org/example/Main.java
+++ TP/src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
-import org.classes.dynamic.*;
 import static org.utils.Ex1.*;
 
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        ex1();
     }
 }
