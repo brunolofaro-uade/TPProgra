a4081cb2a067ba92269c106ad81279db9868cb93
brunolofaro-uade <blofaro@uade.edu.ar>
2023-06-13 11:25:57 -0300

    matrixSum inverted

--- TP/src/main/java/org/example/Main.java
+++ TP/src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ import static org.utils.Ex6.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Excercise1();
-        Excercise6();
+        //Excercise6();
     }
 }
