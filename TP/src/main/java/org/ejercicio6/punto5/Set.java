2053925567b21f3dedff5e56f221e70839956740
Horacio Dalla Valle <dallavalle.h@pm.me>
2023-07-01 18:32:38 -0300

    Add boilerplate code for exercise 6.5

---
+++ TP/src/main/java/org/ejercicio6/punto5/Set.java
@@ -0,0 +1,70 @@
+package org.ejercicio6.punto5;
+
+import java.util.Random;
+
+public class Set implements ISet {
+
+    private final int[] array;
+    private int count;
+
+    public Set() {
+        this.array = new int[10000];
+        this.count = 0;
+    }
+
+    @Override
+    public void add(int a) {
+        for (int i = 0; i < this.count; i++) {
+            if (this.array[i] == a) {
+                return;
+            }
+        }
+
+        this.array[this.count] = a;
+        this.count++;
+    }
+
+    @Override
+    public void remove(int a) {
+        for (int i = 0; i < this.count; i++) {
+            if (this.array[i] == a) {
+                if (i == this.count - 1) {
+                    this.count--;
+                    return;
+                }
+                this.array[i] = this.array[this.count - 1];
+                this.count--;
+            }
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.count == 0;
+    }
+
+    @Override
+    public int choose() {
+        if (this.count == 0) {
+            System.out.println("No se puede elegir un elemento del conjunto vacio");
+            return -1;
+        }
+        int randomIndex = (new Random()).nextInt(this.count);
+        return this.array[randomIndex];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Set set = (Set) o;
+        if (count != set.count) return false;
+
+        for (int i = 0; i < count; i++) {
+            if (array[i] != set.array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
