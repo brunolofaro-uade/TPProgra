e8c5be674d870bfe6f4d74542bf186c7416b592b
Dalla Valle Horacio Daniel Jesus <dallavalle.h@pm.me>
2023-07-03 22:34:26 -0300

    Add exercise 6.4 resolution

---
+++ TP/src/main/java/org/ejercicio6/punto4/Set.java
@@ -0,0 +1,111 @@
+package org.ejercicio6.punto4;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Set implements ISet {
+
+    private Node first;
+    private int count;
+
+    @Override
+    public void add(int a) {
+        if (this.first == null) {
+            this.first = new Node(a, null);
+            this.count++;
+            return;
+        }
+
+        if (this.first.getValue() == a) {
+            this.first.addRepetition();
+            return;
+        }
+
+        Node candidate = this.first;
+        while (candidate.getNext() != null) {
+            candidate = candidate.getNext();
+            if (candidate.getValue() == a) {
+                candidate.addRepetition();
+                return;
+            }
+        }
+        candidate.setNext(new Node(a, null));
+        this.count++;
+    }
+
+    @Override
+    public void remove(int a) {
+        if (this.first == null || (this.first.getNext() == null && this.first.getValue() != a)) {
+            return;
+        }
+
+        if (this.first != null && this.first.getNext() == null) {
+            if (this.first.getValue() == a) {
+                if (this.first.getRepetitionCount() > 0) {
+                    this.first.subtractRepetition();
+                    return;
+                } else {
+                    this.first = null;
+                    this.count--;
+                }
+            }
+            return;
+        }
+
+        if (this.first.getValue() == a) {
+            if (this.first.getRepetitionCount() > 0) {
+                this.first.subtractRepetition();
+                return;
+            } else {
+                this.first = this.first.getNext();
+                this.count--;
+                return;
+            }
+        }
+
+        Node backup = this.first;
+        Node candidate = this.first.getNext();
+
+        while (candidate != null) {
+            if (candidate.getValue() == a) {
+                if (candidate.getRepetitionCount() > 0) {
+                    candidate.subtractRepetition();
+                    return;
+                } else {
+                    backup.setNext(candidate.getNext());
+                    this.count--;
+                    return;
+                }
+            }
+            backup = candidate;
+            candidate = candidate.getNext();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.count == 0;
+    }
+
+    @Override
+    public int choose() {
+        if (this.count == 0) {
+            System.out.println("No se puede elegir un elemento del conjunto vacio");
+            return -1;
+        }
+        int randomIndex = (new Random()).nextInt(this.count);
+        Node candidate = this.first;
+        for (int i = 1; i <= randomIndex; i++) {
+            candidate = candidate.getNext();
+        }
+        return candidate.getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Set set = (Set) o;
+        return count == set.count && Objects.equals(first, set.first);
+    }
+}
